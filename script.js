8bd2c5fe9aafad09f00d76ff58557e83c3f01d7e
TopboySaint <topboysaint1@gmail.com>
2025-04-29 06:06:20 -0700

    implement analyzePunctuation function

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-
-
 function analyzeVowels() {
   const vowels = "aeiou";
   let count = 0;
 
   const sentence = document.getElementById("sentenceInput").value;
-const result = document.getElementById("output");
-const errorMsg = document.getElementById("errorMsg");
+  const result = document.getElementById("output");
+  const errorMsg = document.getElementById("errorMsg");
 
   if (sentence.trim() === "") {
     errorMsg.style.display = "block";
@@ -27,9 +25,9 @@ function analyzeConsonants() {
   let count = 0;
 
   const sentence = document.getElementById("sentenceInput").value;
-const result = document.getElementById("output");
-const errorMsg = document.getElementById("errorMsg");
-  
+  const result = document.getElementById("output");
+  const errorMsg = document.getElementById("errorMsg");
+
   if (sentence.trim() === "") {
     errorMsg.style.display = "block";
     result.innerHTML = "";
@@ -45,20 +43,29 @@ const errorMsg = document.getElementById("errorMsg");
 }
 
 
-function getPunctuationCount(sentence) {
-  const punctuations = ".,!?;:-()[]{}\"'–";
-  let count = 0;
 
-  for (const char of sentence) {
-    if (punctuations.includes(char)) {
-      count++;
+function analyzePunctuation() {
+    const punctuations = ".,!?;:-()[]{}\"'–";
+    let count = 0;
+  
+    const sentence = document.getElementById("sentenceInput").value;
+    const result = document.getElementById("output");
+    const errorMsg = document.getElementById("errorMsg");
+  
+    if (sentence.trim() === "") {
+      errorMsg.style.display = "block";
+      result.innerHTML = "";
+    } else {
+      errorMsg.style.display = "none";
+      for (const char of sentence) {
+        if (punctuations.includes(char)) {
+          count++;
+        }
+      }
+      result.innerHTML = `Number of punctuations: ${count}`;
     }
   }
-  return count;
-}
-
-const punctuationCount = getPunctuationCount("WHAT?!?!?!?!?");
-console.log(`Punctuation Count: ${punctuationCount}`);
+  
 
 function getWordCount(sentence) {
   if (sentence.trim() === "") {
